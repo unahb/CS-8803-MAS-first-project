b5a18495ca6c1b2ebc8da7264f8c9a9270a6fffb
unahb <garg.bhanu1@gmail.com>
2022-01-23 22:35:23 -0500

    async fetchMemes failing

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,10 @@
 
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() {
+void main() async{
   runApp(const MyApp());
 }
 
@@ -13,55 +15,92 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return const MaterialApp(
-      title: 'Startup Name Generator',
-      home: RandomWords(),
+      title: 'Random Meme Viewer',
+      home: RandomMemes(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
+class RandomMemes extends StatefulWidget {
+  const RandomMemes({Key? key}) : super(key: key);
 
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  _RandomMemesState createState() => _RandomMemesState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
+class _RandomMemesState extends State<RandomMemes> {
+  // final _suggestions = <WordPair>[];
+  final _memes = <Meme>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
-  Widget _buildSuggestions() {
+  Future<Widget> _buildSuggestions() async{
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
           if (i.isOdd) return const Divider();
 
           final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
+          if (index >= _memes.length) {
+            for (var i = 0; i < 10; i++) {
+              Meme temp = await fetchMemes();
+              _memes.add(temp);
+            }
           }
-          return _buildRow(_suggestions[index]);
+          return _buildRow(_memes[index]);
         });
   }
 
-  Widget _buildRow(WordPair pair) {
+  Widget _buildRow(Meme meme) {
     return ListTile(
       title: Text(
-        pair.asPascalCase,
+        meme.title,
         style: _biggerFont,
+      ),
+      leading: Image.network(
+        meme.url,
+        width: 100,
+        height: 100,
       ),
     );
   }
 
   @override
-  Widget build(BuildContext context) {
+  Future<Widget> build(BuildContext context) async{
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Startup Name Generator'),
+        title: const Text('Meme Viewer'),
       ),
-      body: _buildSuggestions(),
+      body: await _buildSuggestions(),
     );
+  }
+}
+
+const String url = 'https://meme-api.herokuapp.com/gimme/wholesomememes';
+
+class Meme {
+  final String url;
+  final String title;
+
+  const Meme({required this.url, required this.title});
+
+  factory Meme.fromJson(Map<String, dynamic> json) {
+    return Meme(url: json['url'] as String, title: json['title'] as String);
+  }
+}
+
+Future<Meme> fetchMemes() async {
+  final response = await http
+      .get(Uri.parse('https://meme-api.herokuapp.com/gimme/wholesomememes/'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Meme.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load meme');
   }
 }
