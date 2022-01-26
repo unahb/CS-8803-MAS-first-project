e280898951c4b7edc1868f7f0714309f4c2f18b4
unahb <garg.bhanu1@gmail.com>
2022-01-25 20:10:12 -0500

    infinite memes

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() async{
+void main() {
   runApp(const MyApp());
 }
 
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Random Meme Viewer',
       home: RandomMemes(),
@@ -31,76 +30,73 @@ class RandomMemes extends StatefulWidget {
 }
 
 class _RandomMemesState extends State<RandomMemes> {
-  // final _suggestions = <WordPair>[];
-  final _memes = <Meme>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  List<String> memes = [];
+  ScrollController _scrollController = ScrollController();
 
-  Future<Widget> _buildSuggestions() async{
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider();
-
-          final index = i ~/ 2;
-          if (index >= _memes.length) {
-            for (var i = 0; i < 10; i++) {
-              Meme temp = await fetchMemes();
-              _memes.add(temp);
-            }
-          }
-          return _buildRow(_memes[index]);
-        });
-  }
-
-  Widget _buildRow(Meme meme) {
-    return ListTile(
-      title: Text(
-        meme.title,
-        style: _biggerFont,
-      ),
-      leading: Image.network(
-        meme.url,
-        width: 100,
-        height: 100,
-      ),
-    );
+  @override
+  void initState() {
+    super.initState();
+    fetchFive();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        fetchFive();
+      }
+    });
   }
 
   @override
-  Future<Widget> build(BuildContext context) async{
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Meme Viewer'),
+        title: Text("Random Memes"),
       ),
-      body: await _buildSuggestions(),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        controller: _scrollController,
+        itemCount: memes.length,
+        itemBuilder: (context, index) {
+          return Container(
+            // constraints: BoxConstraints.tightFor(
+            //   height: 150.0,
+            // ),
+            child: Image.network(memes[index], fit: BoxFit.fitWidth),
+          );
+        },
+      ),
     );
   }
-}
 
-const String url = 'https://meme-api.herokuapp.com/gimme/wholesomememes';
+  fetch() async {
+    Map<String, String>? requestHeaders = {
+      "Access-Control-Allow-Origin": "*",
+    };
 
-class Meme {
-  final String url;
-  final String title;
-
-  const Meme({required this.url, required this.title});
-
-  factory Meme.fromJson(Map<String, dynamic> json) {
-    return Meme(url: json['url'] as String, title: json['title'] as String);
+    final response = await http.get(
+        Uri.parse('https://meme-api.herokuapp.com/gimme/wholesomememes/5'),
+        headers: requestHeaders);
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      setState(() {
+        for (int i = 0; i < 5; i++) {
+          memes.add(jsonDecode(response.body)['memes'][i]['url']);
+        }
+      });
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load meme');
+    }
   }
-}
 
-Future<Meme> fetchMemes() async {
-  final response = await http
-      .get(Uri.parse('https://meme-api.herokuapp.com/gimme/wholesomememes/'));
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Meme.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load meme');
+  fetchFive() {
+    fetch();
   }
 }
